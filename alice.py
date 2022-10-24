bb1755ba94af7a041c90fc4f542b74435b278fc9
CGraciolli <cgraciolli@gmail.com>
2022-10-24 14:53:10 +0200

    token

--- alice.py
+++ alice.py
@@ -1,11 +1,11 @@
 from functions import normalize, display_histogram_prob, display_histogram_freq
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import wordpunct_tokenize
 
 alice = open("alice.txt", "r", encoding="utf8")
 text_alice = alice.read()
 alice.close()
 
-alice_tok = word_tokenize(text_alice)
+alice_tok = wordpunct_tokenize(text_alice)
 alice_norm = normalize(alice_tok)
 display_histogram_freq(alice_norm)
 display_histogram_prob(alice_norm)
--- stop_words.py
+++ stop_words.py
@@ -1,8 +1,8 @@
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import wordpunct_tokenize
 
 stop_words = open("english.txt", "r", encoding="utf8")
 text_stop = stop_words.read()
 stop_words.close()
 
-stop_words_list = word_tokenize(text_stop)
+stop_words_list = wordpunct_tokenize(text_stop)
 stop_words_set = set(stop_words_list)
